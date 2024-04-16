f3e03a6c73a4213ecdb0c90565d1defe2e1c850a
ChillingTime <102903069+NotForGetNow@users.noreply.github.com>
2024-04-16 20:21:31 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,26 @@
-const input = document.querySelector('input')
+const input = document.querySelector('input');
+const resultElement = document.createElement('p');
+resultElement.classList.add('d-flex', 'justify-content-center', 'mt-3'); // Добавляем класс 'd-none' к созданному h3
+const resultsDiv = document.querySelector('.results');
+resultsDiv.parentNode.insertBefore(resultElement, resultsDiv.nextSibling);
+
 const btn = document.getElementById('button-addon').addEventListener('click', () => {
-    const inputValue = input.value.trim()
-    const isPalid = palidromCheck(inputValue)
-    console.log(isPalid);
-})
+    const inputValue = input.value.trim();
+    const isPalid = palidromCheck(inputValue);
+    resultElement.textContent = isPalid;
+});
 
 function palidromCheck(str) {
-    str = str.toLowerCase().replace(/[^а-яa-z1-9]/gi, '')
-    const iLast = str.length - 1
+    str = str.toLowerCase().replace(/[^а-яa-z1-9]/gi, '');
+    const iLast = str.length - 1;
     
     if(str === '') {
-        return 'Пустое поле ввода. Введите слово!'
+        return 'Пустое поле ввода. Введите слово!';
     }
     for (let i = 0; i < str.length / 2; i++) {
         if ( str[i] !== str[iLast - i]) {
-            return 'Это не полиндром'
+            return 'Это не полиндром';
         }
     }
-    return 'Это полиндром'
+    return 'Это полиндром';
 }
-
-// console.log(palidromCheck());
